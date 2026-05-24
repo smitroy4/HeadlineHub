cff8f12d684a860a5bb431658de0194cef9583e6
Smit Roy <smitroy22@gmail.com>
2026-05-24 05:32:29 +0530

    Spring Boot backend integrated using Rest Template and Docker

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,21 +1,17 @@
-const API_KEY =
-  import.meta.env.VITE_GNEWS_API_KEY;
-
 const BASE_URL =
-  "https://gnews.io/api/v4/search?q=";
+  "https://headlinehub-backend.onrender.com/api/news?query=";
 
 export async function fetchNews(query) {
 
   try {
 
-    const response = await fetch(
-      `${BASE_URL}${encodeURIComponent(query)}&lang=en&max=25&apikey=${API_KEY}`
-    );
+    const response =
+      await fetch(
+        `${BASE_URL}${encodeURIComponent(query)}`
+      );
 
     const data =
       await response.json();
-
-    console.log(data);
 
     return data.articles || [];
 
